7916d9c86da4872b5e721fb2c4d0c5a1955be6da
e <18790336164@163.com>
2021-01-05 22:24:23 +0800

    开发中 添加vant组件

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,27 +1,15 @@
-// pages/login/index.js
 import {Router, app} from '../page';
 
 Router({
     data: {
 
     },
-    /**
-     * 生命周期函数--监听页面加载
-     */
     onLoad: function (options) {
 
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
     onReady: function () {
 
     },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
     onShow: function () {
 
     },
@@ -30,31 +18,15 @@ Router({
         app.$store.isLogin = true
         app.$router.back();
     },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
     onHide: function () {
 
     },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
     onUnload: function () {
 
     },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
     onPullDownRefresh: function () {
 
     },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
     onReachBottom: function () {
 
     },
